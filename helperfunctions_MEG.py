45bced7d02a19fd6615a30d88d0646bc4efd67d2
Angela I Renton <angela.renton@uqconnect.edu.au>
2024-01-26 07:50:12 +0100

    megupdates

--- helperfunctions_MEG.py
+++ helperfunctions_MEG.py
@@ -20,62 +20,7 @@ class MetaData:
     files = {'MEGprocessed': direct['dataroot'] / 'MEGdataframe.pkl'}
 
     # # %% Feature coding for each stimulation location
-    # # Create dictionary to host information
-    # stim_dict = {'site_number': [], 'eccintricity': [], 'angle': []}
-    #
-    # # loop through sites to assign properties
-    # eccin = 0
-    # for site in range(n_sites):
-    #     # set angle
-    #     if eccin == 0:
-    #         angle = np.mod(45 - 90*site, 360)
-    #     if eccin == 1:
-    #         angle = np.mod(67.5 - 45*(site-4), 360)
-    #     if eccin > 1:
-    #         angle = np.mod(75 - 30*np.mod(site, 12), 360)
-    #
-    #     # set properties
-    #     stim_dict['site_number'].append(site+1)  # site number
-    #     stim_dict['angle'].append(angle)  # site number
-    #     stim_dict['eccintricity'].append(eccin)  # eccintricity
-    #
-    #     # advance eccintricities
-    #     if np.isin(site+1, [4, 12, 24, 36, 48, 60]):
-    #         eccin += 1
-    #
-    # # set visual fields
-    # stim_dict['visfield_horz'] = (np.mod(stim_dict['angle'], 275) < 90).astype(int)
-    # stim_dict['visfield_horz_str'] = np.array(['Left', 'Right'])[stim_dict['visfield_horz']]
-    #
-    # stim_dict['visfield_vert'] = (np.array(stim_dict['angle']) < 180).astype(int)
-    # stim_dict['visfield_vert_str'] = np.array(['Bottom', 'Top'])[stim_dict['visfield_horz']]
-    #
-    # # get cardinal coordinates
-    # stim_dict['xpos'] = (np.array(stim_dict['eccintricity'] ) + 1) * np.cos(np.deg2rad(stim_dict['angle']))
-    # stim_dict['ypos'] = (np.array (stim_dict['eccintricity']) + 1) * np.sin(np.deg2rad(stim_dict['angle']))
-    #
-    # # convert to pandas dataframe
-    # stim_df = pd.DataFrame(stim_dict)
-    #
-    #
-    # # Organise some plotting groups we might want to plot by
-    # horz_groups = {'-3': [57, 58, 45, 46], '-2': [34, 22, 33, 21], '-1': [11, 4, 10, 3],
-    #                '1': [1, 6, 2, 7], '2': [15, 27, 16, 28], '3': [39, 51, 40, 52]}
-    #
-    # vert_groups = {'-3': [55, 54, 43, 42], '-2': [31, 30, 19, 18], '-1': [9, 8, 3, 2],
-    #                '1': [1, 4, 5, 12], '2': [24, 13, 36, 25], '3': [48, 37, 60, 49]}
-    #
-    # # assign horizontal groups
-    # stim_df['horz_group'] = 0
-    # for group in horz_groups:
-    #     for index in horz_groups[group]:
-    #         stim_df.loc[stim_df.site_number == index, 'horz_group'] = int(group)
-    #
-    # # assign vertical groups
-    # stim_df['vert_group'] = 0
-    # for group in vert_groups:
-    #     for index in vert_groups[group]:
-    #         stim_df.loc[stim_df.site_number == index, 'vert_group'] = int(group)
+    behvars = ['cond_id', 'stimsize', 'x_pos', 'y_pos', 'angle', 'x_dir', 'ydir']
 
     def get_subids(self):
         subids = os.listdir(self.direct['dataroot'] / Path('MEG'))
@@ -86,7 +31,7 @@ class MetaData:
 
         # Preallocate
         megdat_dict = {'sub_id': [], 'epoch': [], 'rep': [], 'sensor': [], 'Amplitude (T)': [], 'cond_id': [],
-                       'stimsize': [], 'x_pos': [], 'y_pos': [], 'angle': []}
+                       'stimsize': [], 'x_pos': [], 'y_pos': [], 'angle': [], 'x_dir': [], 'y_dir': []}
 
         # loop through subjects
         for ii_sub, subid in enumerate(subids):
@@ -106,9 +51,10 @@ class MetaData:
             epochs_use = np.where(np.logical_not(np.isin(CONDdat, [10, 20])))[1]
 
             # loop through sites
-            for epoch in epochs_use:
-                for rep in range(2):
-                    for sensor in range(self.n_sensors):
+            for rep in range(2):
+                for sensor in range(self.n_sensors):
+                    for epoch in epochs_use:
+                        megdat_dict['sub_id'].append(subid)
                         megdat_dict['epoch'].append(epoch)
                         megdat_dict['rep'].append(rep)
                         megdat_dict['sensor'].append(sensor)
@@ -116,7 +62,7 @@ class MetaData:
 
                         # condition labels
                         stimuse = STIMdat[epoch, :, :]
-                        megdat_dict['cond_id'].append(CONDdat[:, epoch])
+                        megdat_dict['cond_id'].append(CONDdat[:, epoch][0])
                         megdat_dict['stimsize'].append(stimuse.sum())
                         megdat_dict['x_pos'].append(np.where(stimuse)[0].mean())
                         megdat_dict['y_pos'].append(np.where(stimuse)[1].mean())
@@ -131,68 +77,62 @@ class MetaData:
                         lengths = {l_idx[str(broader)]: np.diff(loc[broader][idx])[0],
                                    l_idx[str(thinner)]: np.diff(loc[thinner][idx])[0]}
                         angle = np.rad2deg(math.atan2(lengths['y'], lengths['x']))
+                        if 10 < angle < 80:
+                            angle = 45
+                        if -10 > angle > -80:
+                            angle = 45
                         megdat_dict['angle'].append(angle)
 
-                        plt.figure()
-                        plt.imshow(stimuse.T)
-                        plt.title('Epoch = ' + str(epoch) + 'lengths x,y = ' + str(lengths['x']) + ',' + str(lengths['y']) + 'angle = ' + str(angle))
+                        # if (rep == 1) & (sensor == 1) & (np.mod(epoch,20)==0):
+                        #     plt.figure()
+                        #     plt.imshow(stimuse.T)
+                        #     plt.title('Epoch = ' + str(epoch) + '| Lengths x,y = ' + str(lengths['x']) + ',' + str(lengths['y']) + '| Angle = ' + str(angle))
 
-            #direction
+        # calculate motion directions
+        xdiff, ydiff = np.diff(megdat_dict['x_pos'], prepend=0), np.diff(megdat_dict['y_pos'], prepend=0) # works because we cycle through epochs in the inner loop and epochs are sequential in time
+        xdiff[np.abs(xdiff) > 6],  ydiff[np.abs(ydiff) > 6] = 0, 0
+        xdiff, ydiff = np.sign(xdiff), np.sign(ydiff)
+        megdat_dict['x_dir'], megdat_dict['y_dir'] = xdiff, ydiff
+
+        # plt.figure()
+        # plt.plot(megdat_dict['x_pos'])
+        # plt.plot(megdat_dict['y_pos'])
+        #
+        # plt.figure()
+        # plt.plot(np.sign(xdiff))
+        # plt.plot(np.sign(ydiff))
 
         megdat_df = pd.DataFrame(megdat_dict)
 
-        # assign to plots to align with our stimulus dataframe
-        # assign horizontal groups
-        eegdat_df['horz_group'] = 0
-        for group in self.horz_groups:
-            for index in self.horz_groups[group]:
-                eegdat_df.loc[eegdat_df.site_id == index, 'horz_group'] = int(group)
-
-        # assign vertical groups
-        eegdat_df['vert_group'] = 0
-        for group in self.vert_groups:
-            for index in self.vert_groups[group]:
-                eegdat_df.loc[eegdat_df.site_id == index, 'vert_group'] = int(group)
-
+        for key in megdat_dict.keys():
+            print(key + str(len(megdat_dict[key])))
         # save data
-        eegdat_df.to_pickle(self.files['EEGprocessed'])
+        megdat_df.to_pickle(self.files['MEGprocessed'])
 
-        return eegdat_df
+        return megdat_df
 
     # Prepare data for PLSC
-    def organise_eegdata(self, eegdat_df, str_behave, grouper=True):
+    def organise_megdata(self, megdat_df, grouper=True):
 
-        # Get positive time values
-        eeg_resampled = eegdat_df.copy()
-        eeg_resampled = eeg_resampled.loc[eeg_resampled['time (s)'] > 0, :]
-        eeg_resampled['time (s)'] = pd.TimedeltaIndex(eeg_resampled['time (s)'],  unit='s')
-
-        # resample
-        grouper = eeg_resampled.groupby(['sub_id', 'site_id', 'ch_name'])
-        eeg_resampled = grouper.resample('0.01S', on='time (s)', group_keys='True').mean()
-        eeg_resampled = eeg_resampled.reset_index()
+        # Get copy of data
+        meg_resampled = megdat_df.copy()
+        meg_resampled = meg_resampled.dropna(axis=0)
 
         # group
         if grouper:
-            eeg_resampled = eeg_resampled.groupby(['site_id', 'ch_name', 'time (s)'])['EEG amp. (µV)'].mean().reset_index()
-            eeg_resampled['sub_id'] = 'sub1'
+            meg_resampled = meg_resampled.groupby(['cond_id', 'epoch', 'sensor', 'rep'])[['stimsize', 'x_pos', 'y_pos', 'angle', 'x_dir', 'y_dir', 'Amplitude (T)']].mean().reset_index()
+            meg_resampled['sub_id'] = 'sub1'
             nsubs = 1
 
-            # save data
-            eeg_resampled.to_pickle(self.direct['resultsroot'] / Path('eegretinotopicmappingdf.pkl'))
         else:
-            nsubs = 28
-            # save data
-            eeg_resampled.to_pickle(self.direct['resultsroot'] / Path('eegretinotopicmappingdf_ungrouped.pkl'))
+            nsubs = self.n_subs
 
-
-
-        return eeg_resampled, nsubs
+        return meg_resampled, nsubs
 
 
 # define PLSC class
 class PLSC:
-    def stack_data(self, nsubs, sets, str_behave, eeg_resampled):
+    def stack_data(self, nsubs, str_behaveuse, meg_resampled):
         ## stack data
         # X = [[cond, ch x times]Pid, [cond, ch x times]Pid ..., [cond, ch x times]pidn] .
         # Y = [[xpos, ypos, eccin]p1cond1, ...]
@@ -205,10 +145,11 @@ class PLSC:
             x_n, y_n = [], []
 
             # cycle through conditions
-            for site in range(sets.n_sites):
+            for epoch in meg_resampled.epoch.unique():
                 # get condition labels
-                ydat = sets.stim_df.loc[sets.stim_df.site_number == (site+1), str_behave].to_numpy()
-                xdat = eeg_resampled.loc[(eeg_resampled['sub_id'] == ('sub' + str(sub+1))) & (eeg_resampled.site_id == (site+1)), ['EEG amp. (µV)']]
+                ydat = meg_resampled.loc[(meg_resampled['sub_id'] == ('sub' + str(sub+1))) & (meg_resampled.epoch == epoch), str_behaveuse].mean().to_numpy().T
+                xdat = meg_resampled.loc[(meg_resampled['sub_id'] == ('sub' + str(sub+1))) & (meg_resampled.epoch == epoch), ['Amplitude (T)']]
+                # question - make sure it's always the same order of sensors etc.
 
                 # resample EEG data
                 x_n.append(xdat.to_numpy())
@@ -219,22 +160,22 @@ class PLSC:
 
         return X_stack, Y_stack
 
-    def normalise(self, X, Y, nsubs, sets):
+    def normalise(self, X_stack, Y_stack, nsubs, sets):
         X_norm, Y_norm = [],[]
         normstats = {'X_mean':[], 'X_ss': []}
         for sub in range(nsubs):
             print('Normalising data for subject: ' + str(sub))
 
             # get data
-            x_n, y_n = np.squeeze(X[sub]), np.squeeze(Y[sub])
-
+            x_n, y_n = np.squeeze(X_stack[sub]), np.squeeze(Y_stack[sub])
+            luse = x_n.shape[0]
             ## Zero mean each column
-            [x_n, y_n] = [dat - np.tile(dat.mean(axis=0).T, (sets.n_sites, 1)) for dat in [x_n, y_n]]
+            [x_n, y_n] = [dat - np.tile(dat.mean(axis=0).T, (luse, 1)) for dat in [x_n, y_n]]
             normstats['X_mean'].append(x_n.mean(axis=0))
 
             # divide by sqrt of sum of squares
             [x_ss, y_ss] = [np.sqrt(np.sum(np.square(dat), axis=0)) for dat in [x_n, y_n]]
-            [x_n, y_n] = [dat/np.tile(ss.T, (sets.n_sites, 1)) for dat, ss in zip([x_n, y_n], [x_ss, y_ss])]
+            [x_n, y_n] = [dat/np.tile(ss.T, (luse, 1)) for dat, ss in zip([x_n, y_n], [x_ss, y_ss])]
             normstats['X_ss'].append(x_ss)
 
             # Store
@@ -268,7 +209,7 @@ class PLSC:
 
         return R
 
-    def compute_SVD(self, R, X_norm, Y_norm, sets, str_behave, nsubs):
+    def compute_SVD(self, R, X_norm, Y_norm, sets, str_behaveuse, nsubs):
         # concatenate into a single matrix (from list form)
         R = np.concatenate(R, axis=0)
         X = np.squeeze(np.concatenate(X_norm, axis=0))
@@ -282,8 +223,8 @@ class PLSC:
 
         # get U dataframe
         U_df = pd.DataFrame(U)
-        U_df['Measure'] = [behave for sub in range(nsubs)for behave in str_behave]
-        U_df['SUB'] = [sub for sub in range(nsubs) for behave in str_behave]
+        U_df['Measure'] = [behave for sub in range(nsubs)for behave in str_behaveuse]
+        U_df['SUB'] = [sub for sub in range(nsubs) for behave in str_behaveuse]
 
         return U, D, V, X, Y, U_df
 
--- runPLSC_MEG.py
+++ runPLSC_MEG.py
@@ -19,7 +19,7 @@ loadMEGfresh = True
 # loadMEGresampled_fresh = False
 
 # full group
-run_fullgroup = False
+run_fullgroup = True
 
 # leave one out cross validation
 run_crossvalidation = False
@@ -31,3 +31,58 @@ plsc = helper.PLSC()
 subs = sets.get_subids()
 
 # %% Load MEG data
+
+if loadMEGfresh:
+    megdat_df = sets.get_eeg()
+else:
+    megdat_df = pd.read_pickle(sets.files['MEGprocessed'])
+
+if run_fullgroup:
+    # set behavioural variables
+    str_behaveuse = [ 'stimsize', 'x_pos', 'y_pos', 'angle', 'x_dir', 'y_dir']
+
+    # get resampled variables
+    meg_resampled, nsubs = sets.organise_megdata(megdat_df, grouper=True) # grouper = average data across participants?
+
+    # %% stack data up across subjects and conditions to form X and Y
+    # X = [[cond, ch x times]Pid, [cond, ch x times]Pid ..., [cond, ch x times]pidn] .
+    # Y = [[cond,[behave values i.e. xpos, ypos]]p1, ..
+    plsc = helper.PLSC()
+    X_stack, Y_stack = plsc.stack_data(nsubs, str_behaveuse, meg_resampled)
+
+    # %% # Normalise
+    # Both X and Y are centered and normalized within each "condition" (subject)
+    # n (i.e., each Xn and Yn is centered and normalized independently,
+    # and the sum of squares of a column in one condition is equal to 1,
+    X_norm, Y_norm, normstats = plsc.normalise(X_stack, Y_stack, nsubs, sets)
+
+    # %% compute R (covariance)
+    R = plsc.compute_covariance(X_norm, Y_norm, sets, nsubs)
+
+    # %% Restructure variables
+    U, D, V, X, Y, U_df = plsc.compute_SVD( R, X_norm, Y_norm,sets, str_behaveuse, nsubs)
+
+    #%% Compute latent variables
+    Lx, Ly = plsc.compute_latentvars(sets, X, Y_norm, U_df, U, V, D, nsubs)
+    # np.savez(sets.direct['resultsroot'] / Path('SVDcomponents.npz'), U=U, D=D,V=V, Lx=Lx, stim_df=setes.stim_df)
+
+    # %% Visualise latent vars
+    sets.nsubs = nsubs
+    plot.behave_saliances(U_df, sets)
+    plot.latentspace_2d(sets, Lx)
+    plot.latentspace_3d(sets, Lx)
+
+    str_behaveuse = [ 'stimsize', 'x_pos', 'y_pos', 'angle', 'x_dir', 'y_dir']
+    fig, ax = plt.subplots(1,3, figsize=(10,4), layout='tight')
+    ax[0].scatter(Lx[:, 1],Lx[:, 0],c=Y[:,0],cmap="coolwarm", s=50)
+    ax[1].scatter(Lx[:, 1],Lx[:, 0],c=Y[:,1],cmap="coolwarm", s=50)
+    ax[2].scatter(Lx[:, 1],Lx[:, 2],c=Y[:,2],cmap="coolwarm", s=50)
+
+    fig, ax = plt.subplots(1,3, figsize=(10,4), layout='tight')
+    ax[0].scatter(Lx[:, 4],Lx[:, 3],c=Y[:,5],cmap="coolwarm", s=50)
+    ax[1].scatter(Lx[:, 1],Lx[:, 4],c=Y[:,4],cmap="coolwarm", s=50)
+    ax[2].scatter(Lx[:, 3],Lx[:, 5],c=Y[:,3],cmap="coolwarm", s=50)
+
+    # plot reconstructed latent space vars
+    eeg_V = plot.latentspaceERPs(sets, eeg_resampled, V)
+    plot.animatecomponents(eeg_V, info, sets, component='V0')
